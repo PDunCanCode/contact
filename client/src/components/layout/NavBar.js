70592805f6976dee08cccedd1de26d3e2925182d
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-20 16:01:28 -0800

    client side ... not compiling

---
+++ client/src/components/layout/NavBar.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const NavBar = ({ title, icon }) =>{
+    return (
+        <div className="navbar bg-primary">
+            <h1>
+                <i className={icon} /> {title}
+            </h1>
+
+        </div>
+    )
+}
+
+NavBar.PropTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string,
+}
+
+NavBar.defaultProps = {
+    title: 'Contact',
+    icon: 'fas fa-id-card-alt'
+}
+
+export default Navbar;
