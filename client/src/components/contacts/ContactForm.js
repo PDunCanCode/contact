10f88557e15551468f210a0fef84fa5d35cf3dba
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-21 15:25:51 -0800

    contactForm functionality updated

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,15 +1,52 @@
-import React, { useState } from 'react';
+
+import React, { useState, useContext, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
-    const [contact, setContact] = useState({
+  const contactContext = useContext(ContactContext);
+
+  const { addContact, updateContact, clearCurrent, current } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
         name: '',
         email: '',
         phone: '',
-        type: 'personal',
-    });
-    const { name, email, phone, type } = contact;
+        type: 'personal'
+      });
+    }
+  }, [contactContext, current]);
 
-    return (
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal'
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = e =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
+  };
+
+  const clearAll = () => {
+    clearCurrent();
+  };
+
+  return (
         <form onSubmit={onSubmit}>
           <h2 className='text-primary'>
             {current ? 'Edit Contact' : 'Add Contact'}
@@ -70,4 +107,4 @@ const ContactForm = () => {
       );
     };
     
-    export default ContactForm;
+export default ContactForm;
