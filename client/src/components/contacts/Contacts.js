31d217a2315b1315bdcd269dd776fda64ae1fe78
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-21 10:18:49 -0800

    ContactItem updated, the buttons they do nothing

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,13 +1,18 @@
 import React, { Fragment, useContext } from 'react';
+import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    return (
-        <div>
+    const { contacts } = contactContext;
 
-        </div>
+    return (
+        <Fragment>
+            {contacts.map(contact => (
+                <ContactItem key={contact.id} contact={contact} />
+            ))}
+        </Fragment>
     )
 }
 
