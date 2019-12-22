9624e7470dedb72e0ef04bb334aba9478c8f9b88
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-21 19:51:02 -0800

    Filtering Contacts

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,25 @@
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/ContactContext';
+
+const ContactFilter = () => {
+    const contactContext = useContext(ContactContext);
+    const text = useRef('');
+
+    const onChange = e => {
+        if(text.current.value !== ''){
+            contactContext.filterContacts(e.target.value);
+        } else {
+            contactContext.clearFilter();
+        }
+    }
+    return (
+        <form>
+            <input ref={text} placeholder="Filter Contacts..." 
+            onChange={onChange}/>
+        </form>
+
+
+    )
+}
+
+export default ContactFilter;
