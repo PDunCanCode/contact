f4d2c40041b48002f3a57e43c3d0e66b6d9fbaa9
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-21 20:03:02 -0800

    Contacts Filtered

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -5,13 +5,21 @@ const ContactFilter = () => {
     const contactContext = useContext(ContactContext);
     const text = useRef('');
 
-    const onChange = e => {
-        if(text.current.value !== ''){
-            contactContext.filterContacts(e.target.value);
-        } else {
-            contactContext.clearFilter();
-        }
+    const { filterContacts, clearFilter, filtered } = contactContext;
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
     }
+  });
+
+  const onChange = e => {
+    if (text.current.value !== '') {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
     return (
         <form>
             <input ref={text} placeholder="Filter Contacts..." 
