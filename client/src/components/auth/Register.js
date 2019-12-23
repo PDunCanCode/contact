8b347e70584b75cc51b4f00e0414b87fef435d62
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-22 21:56:48 -0800

    jwt auth, ... jeez.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,47 +1,58 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
-
-const Register = () => {
+const Register = props => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
   const { register, error, clearErrors, isAuthenticated } = authContext;
 
-    const [user, setUser] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
-    });
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
 
-    const { name, email, password, password2 } = user;
-    const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
-    const onSubmit = e => {
-      e.preventDefault();
-      if (name === '' || email === '' || password === '') {
-        setAlert('Please enter all fields', 'danger');
-      } else if (password !== password2) {
-        setAlert('Passwords do not match', 'danger');
-      } else {
-        register({
-          name,
-          email,
-          password
-        });
-      }
-    };
-  
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
 
-    return (
-        <div className='form-container'>
-            <h1>
-                Account <span className="text-primary">Register</span>
-            </h1>
-            <form onSubmit={onSubmit}>
+  const { name, email, password, password2 } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      register({
+        name,
+        email,
+        password
+      });
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
           <input
